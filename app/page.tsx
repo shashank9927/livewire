28e3fee0cd9a23bc69a69f48dc52829a796942bd
shashank9927 <shashankat99@gmail.com>
2026-01-10 21:15:12 +0100

    Wire up main app

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,66 @@
+'use client'
 
+import { useState, useEffect } from 'react'
+import ChatRoom from './components/ChatRoom'
+import JoinCreate from './components/JoinCreate'
+import { useWebSocket } from './hooks/useWebSocket'
+import { toast } from 'sonner'
+import LiveWireIcon from './components/icons/LiveWireIcon'
 
 export default function Home() {
-  return (
-    <main>
+  const [roomId, setRoomId] = useState<string | null>(null)
+  const { connectionStatus, lastMessage, sendMessage } = useWebSocket()
 
+  useEffect(() => {
+    if (lastMessage) {
+      if (lastMessage.type === 'roomJoined') {
+        setRoomId(lastMessage.payload.roomId)
+      }
+      else if (lastMessage.type === 'error') {
+        toast.error(`Error: ${lastMessage.payload.message}`)
+      }
+    }
+  }, [lastMessage])
+
+  const handleCreateRoom = () => {
+    sendMessage({ type: 'create', payload: {} })
+  }
+
+  const handleSendMessage = (message: string) => {
+    if (roomId) {
+      sendMessage({ type: 'chat', payload: { message, roomId } })
+    }
+  }
+
+  const handleLeaveRoom = () => {
+    if (roomId) {
+      sendMessage({ type: 'leave', payload: { roomId } })
+      setRoomId(null)
+      toast.success('Left room successfully')
+    }
+  }
+
+  return (
+    <main className="flex min-h-[90vh] flex-col items-center justify-center p-0 max-sm:p-4">
+      <h1 className="text-4xl font-bold mb-8 flex space-x-3 items-center">
+        <span><LiveWireIcon size={32} color="#01ff04" /></span>
+        <span>LiveWire</span>
+      </h1>
+      {roomId ? (
+        <ChatRoom
+          roomId={roomId}
+          connectionStatus={connectionStatus}
+          lastMessage={lastMessage}
+          onSendMessage={handleSendMessage}
+          onLeaveRoom={handleLeaveRoom}
+        />
+      ) : (
+        <JoinCreate
+          connectionStatus={connectionStatus}
+          onCreateRoom={handleCreateRoom}
+          sendMessage={sendMessage}
+        />
+      )}
     </main>
-  );
+  )
 }
